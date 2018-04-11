73abab08b76ac47c41e411125fc1f1d4b54f3342
Anna Royer <anna.royer@colorado.edu>
2018-04-11 09:38:58 -0600

    try adding start and end dates both coming out same

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,7 @@
 class MostDangerousDayController < ApplicationController
   def index
+    byebug
+    @start_date = Time.new(params[:start_date]).strftime('%B %-d, %Y')
+    @end_date = Time.new(params[:end_date]).strftime('%B %-d, %Y')
   end
 end
