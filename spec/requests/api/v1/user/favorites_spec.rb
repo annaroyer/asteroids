1479306f16a9d8b36adaa6b1ee3043503ddd813a
Anna Royer <anna.royer@colorado.edu>
2018-04-11 11:52:07 -0600

    update to reference the first of returned json

--- spec/requests/api/v1/user/favorites_spec.rb
+++ spec/requests/api/v1/user/favorites_spec.rb
@@ -3,23 +3,35 @@ require 'rails_helper'
 describe 'As a registered user' do
   context "When I send a 'GET' request to '/api/v1/user/favorites?api_key=abc123" do
     scenario 'Then I should receive a JSON response' do
-      get '/api/v1/user/favorites?api_key=abc123'
+      uncle_jesse = User.create(name: "Jesse Katsopolis", email: "uncle.jesse@example.com")
+      uncle_jesse.create_api_key(value: "abc123")
+      uncle_jesse.favorites.create(neo_reference_id: "2153306")
 
-      expect(response).to eq(favorites)
+      get "/api/v1/user/favorites?api_key=abc123"
+
+      expect(response.status).to eq 200
+
+      favorites = JSON.parse(response.body, symbolize_names: true)
+
+      expect(favorites.first[:id]).to eq(1)
+      expect(favorites.first[:neo_reference_id]).to eq("2153306")
+      expect(favorites.first[:user_id]).to eq(1)
+      expect(favorites.first[:asteriod][:name]).to eq("153306 (2001 JL1)")
+      expect(favorites.first[:asteriod][:is_potentially_hazardous_asteroid]).to eq(false)
     end
   end
 end
 
-def favorites
-  [
-    {
-      "id":1,
-      "neo_reference_id": "2153306",
-      "user_id": 1,
-      "asteroid": {
-        "name": "153306 (2001 JL1)",
-        "is_potentially_hazardous_asteroid": false,
-      }
-    }
-  ]
-end
+  #
+  # [
+  #   {
+  #     "id":1,
+  #     "neo_reference_id": "2153306",
+  #     "user_id": 1,
+  #     "asteroid": {
+  #       "name": "153306 (2001 JL1)",
+  #       "is_potentially_hazardous_asteroid": false,
+  #     }
+  #   }
+  # ]
+  #
