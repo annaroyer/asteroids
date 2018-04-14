5e46f0843f671c31d8ba47b29f9f4d494f78b6cf
Anna Royer <anna.royer@colorado.edu>
2018-04-14 16:18:28 -0600

    update spec for correct date format

--- spec/features/user_can_see_most_dangerous_day_spec.rb
+++ spec/features/user_can_see_most_dangerous_day_spec.rb
@@ -22,25 +22,3 @@ describe "A guest user visits the root page and enters a start date and end date
     expect(page).to have_content("NEO Reference ID: 3794979")
   end
 end
-
-#
-# As a guest user
-# When I visit "/"
-# And I enter "2018-01-01" into the start date
-# And I enter "2018-01-07" into the end date
-# And I click "Determine Most Dangerous Day"
-#
-# Then I should be on "/most_dangerous_day"
-# And I should see a heading for "Most Dangerous Day"
-# And I should see "January 1, 2018 - January 7, 2018"
-# And I should see a heading for the most dangerous day in that range "January 1, 2018"
-# And I should see 3 asteroids in that list
-#
-# And I should see "Name: (2014 KT76)"
-# And I should see "NEO Reference ID: 3672906"
-#
-# And I should see "Name: (2001 LD)"
-# And I should see "NEO Reference ID: 3078262"
-#
-# And I should see "Name: (2017 YR1)"
-# And I should see "NEO Reference ID: 3794979"
