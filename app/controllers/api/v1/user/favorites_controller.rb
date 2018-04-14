d313a6862a7b1462f4b303ebc1711c6d8d71016b
Anna Royer <anna.royer@colorado.edu>
2018-04-14 17:31:58 -0600

    change to only call current_user.favorites in controller

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::User::FavoritesController < ApiBaseController
   def index
-    favorites = current_user.favorites.map do |fav|
-      NasaFavorite.new(fav)
-    end
-    render json: favorites, each_serializer: NasaFavoriteSerializer
+    render json: current_user.favorites
   end
 end
