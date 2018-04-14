ed7ba3dff48baacb61b3a901fa40508f24ef1627
Anna Royer <anna.royer@colorado.edu>
2018-04-14 15:28:49 -0600

    change to only call single instance variable

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,7 +1,10 @@
 class MostDangerousDayController < ApplicationController
   def index
-    @start_date = Time.parse(params[:start_date]).strftime('%B %e, %Y')
-    @end_date = Time.parse(params[:end_date]).strftime('%B %e, %Y')
-    @most_dangerous_day = NasaService.most_dangerous_day(params[:start_date], params[:end_date])
+    @nasa_days = NasaDayPresenter.new(dates)
   end
+
+  private
+    def dates
+      params.permit(:start_date, :end_date)
+    end
 end
