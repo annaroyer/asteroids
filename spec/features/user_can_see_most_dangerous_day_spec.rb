c615b9a5d38704bee41843086c64a0dc65c65485
Anna Royer <anna.royer@colorado.edu>
2018-04-14 16:38:05 -0600

    add stub to user sees most dangerous day feature spec

--- spec/features/user_can_see_most_dangerous_day_spec.rb
+++ spec/features/user_can_see_most_dangerous_day_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe "A guest user visits the root page and enters a start date and end date." do
   scenario "The resulting page will display the most dangerous day in that range based on which day has the most `is_potentially_dangerous_asteroid` set to `true`." do
+    days_in_range = File.open('./spec/fixtures/days_in_range.json')
+    stub_request(:get, "https://api.nasa.gov/neo/rest/v1/feed?api_key=#{ENV['NASA_API_KEY']}&end_date=2018-01-07&start_date=2018-01-01")
+      .to_return(status: 200, body: days_in_range, headers: {})
+
     visit '/'
 
     fill_in :start_date, with: '2018-01-01'
