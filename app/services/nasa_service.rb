4a20eaf6eacaabb3cca9d92c16c4e7fb40155b0f
Anna Royer <anna.royer@colorado.edu>
2018-04-14 16:33:20 -0600

    remove unused methods

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -1,20 +1,8 @@
 class NasaService
 
-  def self.most_dangerous_day(start_date, end_date)
-    get_nasa_days(start_date: start_date, end_date: end_date).max do |a, b|
-      a.near_earth_objects.count <=> b.near_earth_objects.count
-    end
-  end
-
   def self.asteroid(neo_reference_id)
     raw_asteroid = get_url('neo', {neo_reference_id: neo_reference_id})
     Asteroid.new(raw_asteroid)
-  end
-
-  def self.get_nasa_days(params)
-    get_url('feed', params)[:near_earth_objects].map do |day|
-      NasaDay.new(day)
-    end
   end
 
   def self.get_url(url, params={})
