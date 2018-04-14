ad5664f778c372a50d75a322b39745cfd80c0088
Anna Royer <anna.royer@colorado.edu>
2018-04-14 17:32:16 -0600

    add method asteroid to favorite modle

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :user
+
+  def asteroid
+    raw_asteroid = NasaService.get_url("neo/#{neo_reference_id}")
+    Asteroid.new(raw_asteroid)
+  end
 end
