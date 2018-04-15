4152553720d3b09621e31e52925136b595965d35
Anna Royer <anna.royer@colorado.edu>
2018-04-14 18:08:38 -0600

    update test to reflect unexpected id values

--- spec/requests/api/v1/user/favorites_spec.rb
+++ spec/requests/api/v1/user/favorites_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 describe 'As a registered user' do
-  context "When I send a 'GET' request to '/api/v1/user/favorites?api_key=abc123" do
-    before(:each) do
-      uncle_jesse = User.create(name: "Jesse Katsopolis", email: "uncle.jesse@example.com")
-      uncle_jesse.create_api_key(value: "abc123")
-      uncle_jesse.favorites.create(neo_reference_id: "2153306")
-      asteroid = File.open('./spec/fixtures/asteroid.json')
-      stub_request(:get, "https://api.nasa.gov/neo/rest/v1/neo/2153306?api_key=#{ENV['NASA_API_KEY']}")
-        .to_return(status: 200, body: asteroid, headers: {})
-      end
 
+  context "When I send a 'GET' request to '/api/v1/user/favorites?api_key=abc123" do
     scenario 'Then I should receive a JSON response' do
       uncle_jesse = User.create(name: "Jesse Katsopolis", email: "uncle.jesse@example.com")
       uncle_jesse.create_api_key(value: "abc123")
       uncle_jesse.favorites.create(neo_reference_id: "2153306")
+
+      asteroid = File.open('./spec/fixtures/asteroid.json')
+      stub_request(:get, "https://api.nasa.gov/neo/rest/v1/neo/2153306?api_key=#{ENV['NASA_API_KEY']}")
+      .to_return(status: 200, body: asteroid, headers: {})
+
 
       get "/api/v1/user/favorites?api_key=abc123"
 
@@ -32,6 +29,13 @@ describe 'As a registered user' do
 
   context 'When I send a POST request to "/api/v1/user/favorites" with an "api_key" of "abc123" and a "neo_reference_id" of "2021277"' do
     scenario 'Then I should receive a JSON response' do
+      uncle_jesse = User.create(name: "Jesse Katsopolis", email: "uncle.jesse@example.com")
+      uncle_jesse.create_api_key(value: "abc123")
+
+      new_asteroid = File.open('./spec/fixtures/created_asteroid.json')
+      stub_request(:get, "https://api.nasa.gov/neo/rest/v1/neo/2021277?api_key=#{ENV['NASA_API_KEY']}")
+      .to_return(status: 200, body: new_asteroid, headers: {})
+
       post "/api/v1/user/favorites?api_key=abc123&neo_reference_id=2021277"
 
       expect(response.status).to eq 200
@@ -40,7 +44,7 @@ describe 'As a registered user' do
 
       expect(favorite[:id]).to eq(2)
       expect(favorite[:neo_reference_id]).to eq("2021277")
-      expect(favorite[:user_id]).to eq(1)
+      expect(favorite[:user_id]).to eq(uncle_jesse.id)
       expect(favorite[:asteroid][:name]).to eq("21277 (1996 TO5)")
       expect(favorite[:asteroid][:is_potentially_hazardous_asteroid]).to eq(false)
     end
